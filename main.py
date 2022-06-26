d47c129d56c26b9ce6cb029b53f2804338198497
CameronCooke123 <camo246810@yahoo.com>
2022-06-26 01:04:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,30 +27,51 @@ def main ():
                 while True:
                     select_command = input(
 """Available Commands: 
-    1: Create New Product
-    2: Create New Warehouse
-    3: Add Stock to Warehouse
-    4: View Items in Warehouse by Name
-    5: View Items in Warehouse by Type
+     1: Create/Modify Product
+     2: Delete Product
+     3: Create New Warehouse
+     4: Add Stock to Warehouse
+     5: View Items in Warehouse by Name
+     6: View Items in Warehouse by Type
+     7: Add/Modify Product Price
+     8: Delete Product Price
+     9: Add/Modify Customer Address
+    10: Delete Customer Address
+    11: Add/Modify Customer Credit Card
+    12: Delete Customer Credit Card
     e: Exit
 Please Enter a Command: """)
                     if select_command == "1":
-                        sql_string = CreateNewProduct()
+                        sql_string = CreateModifyProduct()
                     elif select_command == "2":
-                        sql_string = CreateWarehouse()
+                        sql_string = DeleteProduct()
                     elif select_command == "3":
-                        sql_string = AddStock()
+                        sql_string = CreateWarehouse()
                     elif select_command == "4":
-                        sql_string = ViewProductsInWarehouse("name")
+                        sql_string = AddStock()
                     elif select_command == "5":
+                        sql_string = ViewProductsInWarehouse("name")
+                    elif select_command == "6":
                         sql_string = ViewProductsInWarehouse("type")
+                    elif select_command == "7":
+                        sql_string = AddModifyPrice()
+                    elif select_command == "8":
+                        sql_string = DeletePrice()
+                    elif select_command == "9":
+                        sql_string = AddModifyCustAddr()
+                    elif select_command == "10":
+                        sql_string = DeleteCustAddr()
+                    elif select_command == "11":
+                        sql_string = AddModifyCC()
+                    elif select_command == "12":
+                        sql_string = DeleteCC()
                     else:
                         break
 
                     cur.execute(sql_string)
                     conn.commit()
 
-                    if select_command == "4" or select_command == "5":
+                    if select_command == "5" or select_command == "6":
                         print(f"{'Product ID':20}{'Warehouse ID':20}{'Number In Stock':20}{'Product Name':20}{'Product Category':20}{'Product Type':20}{'Product Size':20}")
                         for result in cur.fetchall():
                             print(f"{str(result[0]):20}{str(result[1]):20}{str(result[2]):20}{str(result[3]):20}{str(result[4]):20}{str(result[5]):20}{str(result[6]):20}")
@@ -67,15 +88,24 @@ Please Enter a Command: """)
         if conn is not None:
             conn.close()
 
-def CreateNewProduct ():
+def CreateModifyProduct ():
     pid = input("Enter product ID: ")
     name = input("Enter product name: ")
     category = input("Enter product category: ")
     prod_type = input("Enter product type: ")
     size = input("Enter product size: ")
 
-    return "INSERT INTO product VALUES ('" + pid + "', '" + name + "', '" + category + "', '" + prod_type + "', " + \
-           size + ");"
+    return "INSERT INTO product " \
+           "VALUES ('%s', '%s', '%s', '%s', '%s') " \
+           "ON CONFLICT (pid) DO UPDATE " \
+           "SET name = '%s', category = '%s', type = '%s', size = '%s';" \
+           % (pid, name, category, prod_type, size, name, category, prod_type, size)
+
+def DeleteProduct ():
+    pid = input("Enter product ID: ")
+    return "DELETE FROM product " \
+           "WHERE pid = '%s'" \
+           % (pid)
 
 def CreateWarehouse ():
     whid = input("Enter warehouse ID: ")
@@ -109,6 +139,75 @@ def ViewProductsInWarehouse (byWhat):
     sql_string += ";"
 
     return sql_string
+
+def AddModifyPrice ():
+    pid = input("Enter product ID: ")
+    state = input("Enter the state: ")
+    price = input("Enter the dollar amount: ")
+    return "INSERT INTO prod_price " \
+           "VALUES ('%s', '%s', '%s') " \
+           "ON CONFLICT (pid, state) DO UPDATE " \
+           "SET state = '%s', dollaramt = '%s';" \
+           % (pid, state, price, state, price)
+
+def DeletePrice ():
+    pid = input("Enter product ID: ")
+    state = input("Enter the state: ")
+    return "DELETE FROM prod_price " \
+           "WHERE pid = '%s' and state = '%s'" \
+           % (pid, state)
+
+def AddModifyCustAddr ():
+    cid = input("Enter the customer ID: ")
+    st_addr = input("Enter the street address: ")
+    city = input("Enter the city: ")
+    state = input("Enter the state: ")
+    zip_code = input("Enter the zip code: ")
+    return "INSERT INTO cust_addr " \
+           "VALUES ('%s', '%s', '%s', '%s', '%s') " \
+           "ON CONFLICT (cid) DO UPDATE " \
+           "SET staddr = '%s', city = '%s', state = '%s', zipcode = '%s';" \
+           % (cid, st_addr, city, state, zip_code, st_addr, city, state, zip_code)
+
+def DeleteCustAddr ():
+    cid = input("Enter the customer ID: ")
+    st_addr = input("Enter the street address: ")
+    city = input("Enter the city: ")
+    state = input("Enter the state: ")
+    zip_code = input("Enter the zip code: ")
+    return "DELETE FROM cust_addr " \
+           "WHERE cid = '%s' " \
+           "AND staddr = '%s' " \
+           "AND city = '%s' " \
+           "AND state = '%s' " \
+           "AND zipcode = '%s';" \
+           % (cid, st_addr, city, state, zip_code)
+
+def AddModifyCC ():
+    cid = input("Enter customer ID: ")
+    ccNum = input("Enter credit card number: ")
+    secCode = input("Enter security code: ")
+    exp = input("Enter expiration date: ")
+    name = input("Enter the name on card: ")
+    line1 = "INSERT INTO creditcard " \
+            "VALUES ('%s', '%s', '%s', '%s') " \
+            "ON CONFLICT (mainNumber) DO UPDATE " \
+            "SET seccode = '%s', expirydate = '%s', nameoncard = '%s';" \
+            % (ccNum, secCode, exp, name, secCode, exp, name)
+    return line1 + "INSERT INTO cust_cc " \
+                   "VALUES ('%s', '%s') " \
+                   "ON CONFLICT (mainnumber) DO UPDATE " \
+                   "SET cid = '%s';" \
+                   % (ccNum, cid, cid)
+
+def DeleteCC ():
+    ccNum = input("Enter the credit card number: ")
+    line1 = "DELETE FROM cust_cc " \
+            "WHERE mainnumber = '%s';" \
+            % (ccNum)
+    return line1 + "DELETE FROM creditcard " \
+                   "WHERE mainnumber = '%s';" \
+                   % (ccNum)
 
 create_tables_code = '''
         CREATE TABLE IF NOT EXISTS Customer (
@@ -183,16 +282,16 @@ create_tables_code = '''
           city varchar,
           state char(2),
           zipCode varchar,
-          PRIMARY KEY (cid),
-          FOREIGN KEY (cid) references Customer,
-          FOREIGN KEY (stAddr, city, state, zipCode) references Address
+          PRIMARY KEY (cid)
+          --FOREIGN KEY (cid) references Customer,
+          --FOREIGN KEY (stAddr, city, state, zipCode) references Address
         );
         CREATE TABLE IF NOT EXISTS Cust_CC (
           mainNumber numeric(16),
           cid int,
           PRIMARY KEY (mainNumber),
-          FOREIGN KEY (mainNumber) references CreditCard,
-          FOREIGN KEY (cid) references Customer
+          FOREIGN KEY (mainNumber) references CreditCard
+          --FOREIGN KEY (cid) references Customer
         );
         CREATE TABLE IF NOT EXISTS CC_Billing_Addr (
           mainNumber numeric(16),
@@ -264,10 +363,11 @@ create_tables_code = '''
           FOREIGN KEY (name) references Supplier,
           FOREIGN KEY (pid) references Product
         );
+        --DROP TABLE Prod_Price;
         CREATE TABLE IF NOT EXISTS Prod_Price (
           pid int,
           state char(2),
-          dollarAmt int CHECK (dollarAmt > 0),
+          dollarAmt real CHECK (dollarAmt > 0),
           PRIMARY KEY (pid, state),
           FOREIGN KEY (pid) references Product
         );
